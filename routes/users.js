ac2fb8254f57687f2702a66c579423eedb71bfe6
Haneul Bae <ver.753@gmail.com>
2021-10-24 13:18:41 +0900

    test database connection

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,11 @@
+var initializeApp = require('firebase/app');
+var fDatabase = require('firebase/database');
+var admin = require('firebase-admin');
+var fConfig = require('../Config/config.js');
+
+let fApp = initializeApp.initializeApp(fConfig);
+let databaseRef = fDatabase.getDatabase();
+
 var express = require('express');
 var router = express.Router();
 
@@ -11,6 +19,13 @@ router.get('/', function(req, res, next) {
     name : who_name,
     description : who_description
   })
+});
+
+router.get('/test', function(req, res, next) {
+  fDatabase.set(fDatabase.ref(databaseRef, 'Godegi'), { like : "cat" }, function(e) {
+    if (e) alert("error");
+    else res.send("success!");
+  });
 });
 
 module.exports = router;
